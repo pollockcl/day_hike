93a82601da9a0adc1db1ebdae30aaae336e5204e
pollockcl <pollock.clee@gmail.com>
2018-04-13 11:03:06 -0600

    maybe finished last user story?

--- spec/features/trails/can_see_show_spec.rb
+++ spec/features/trails/can_see_show_spec.rb
@@ -23,6 +23,31 @@ context 'as a visitor' do
 
       expect(page).to have_content(trail.total_hikes)
     end
+
+    scenario 'see hikes ' do
+      trail = Trail.create(name: 'Anakin', address: 'Hates Sand', length: 20)
+      hike1 = trail.trips.create(name: 'Anakin', start_date: Time.now, end_date: Time.now)
+      hike2 = trail.trips.create(name: 'Obi Wan', start_date: Time.now + 300000, end_date: Time.now + 300000)
+      hike3 = trail.trips.create(name: 'General Grievious', start_date: Time.now + 900000, end_date: Time.now + 900000)
+
+      visit trail_path(trail)
+
+
+      expect(page).to have_content(hike1.name)
+      expect(page).to have_content(hike1.start_date)
+      expect(page).to have_content(hike1.end_date)
+
+
+      expect(page).to have_content(hike2.name)
+      expect(page).to have_content(hike2.start_date)
+      expect(page).to have_content(hike2.end_date)
+
+
+      expect(page).to have_content(hike3.name)
+      expect(page).to have_content(hike3.start_date)
+      expect(page).to have_content(hike3.end_date)
+
+    end
   end
 end
 
