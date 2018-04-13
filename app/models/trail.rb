bddbc64d2eb15fcff516624372d079debc5a116b
pollockcl <pollock.clee@gmail.com>
2018-04-13 10:56:24 -0600

    total hikes

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,4 +1,8 @@
 class Trail < ApplicationRecord
   has_many :trip_trails
   has_many :trips, through: :trip_trails
+
+  def total_hikes
+    trips.size
+  end
 end 
--- spec/features/trails/can_see_show_spec.rb
+++ spec/features/trails/can_see_show_spec.rb
@@ -11,6 +11,18 @@ context 'as a visitor' do
       expect(page).to have_content(trail.address)
       expect(page).to have_content(trail.length)
     end
+
+    scenario 'see trail hikes ' do
+      trail = Trail.create(name: 'Anakin', address: 'Hates Sand', length: 20)
+      hike1 = trail.trips.create(name: 'Anakin', start_date: Time.now, end_date: Time.now)
+      hike2 = trail.trips.create(name: 'Obi Wan', start_date: Time.now + 300000, end_date: Time.now + 300000)
+      hike3 = trail.trips.create(name: 'General Grievious', start_date: Time.now + 900000, end_date: Time.now + 900000)
+
+      visit trail_path(trail)
+
+
+      expect(page).to have_content(trail.total_hikes)
+    end
   end
 end
 
