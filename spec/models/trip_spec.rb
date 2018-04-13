cce1f5551be50cc57103f7c5b5d39887f33fccd1
pollockcl <pollock.clee@gmail.com>
2018-04-13 10:59:44 -0600

    missing test and tried to fix the naming issue I had on trip_spec and trail_spec.

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -7,4 +7,16 @@ describe Trip do
       expect(trip).to respond_to(:trails)
     end
   end
+
+  describe 'instance_method' do
+    it '.total_hikes' do
+      trail1 = Trail.create(name: 'Obi Wan', length: 3, address: 'Hello There')
+
+      trail1.trips.create(name: 'Anakin', start_date: Time.now, end_date: Time.now)
+
+      trail1.trips.create(name: 'Anakin', start_date: Time.now, end_date: Time.now)
+
+      expect(trail1.total_hikes).to eq(2)
+    end
+  end
 end
